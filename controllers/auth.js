e7f630404dff50fa8882c8d35844e76c21f4bac2
Shashank Gupta <shashankgupta9248@gmail.com>
2023-04-28 07:30:25 +0530

    updates

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,8 @@
-const User = require('../models/User')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const secret = process.env.JWT_SECRET
+const secret = process.env.JWT_SECRET;
 
 const register = async (req, res) =>{
     try {
@@ -11,39 +11,39 @@ const register = async (req, res) =>{
             name: name,
             email: email,
             password: password
-        })
+        });
 
-        res.status(200).json({msg: "User created"})
+        res.status(200).json({msg: "User created"});
 
     } catch (error) {
-        res.status(500).json({error: error})
+        res.status(500).json({error: error});
     }
 }
 
 const login = async (req, res) =>{
     try {
         const { email, password } = req.body;
-        const loggedInUser = await User.findOne({email: email})
+        const loggedInUser = await User.findOne({email: email});
 
         if(loggedInUser){
-            isMatch = await bcrypt.compare(password, loggedInUser.password)
+            isMatch = await bcrypt.compare(password, loggedInUser.password);
             console.log(isMatch);
-            console.log(loggedInUser)
+            console.log(loggedInUser);
     
             if(isMatch){
-                const token = jwt.sign({loggedInUser}, secret )
-                res.status(200).json({msg: "Logged in successfully", token})
+                const token = jwt.sign({loggedInUser}, secret );
+                res.status(200).json({msg: "Logged in successfully", token});
             }
             else{
-                res.status(400).json({msg: "Invalid password for the above username! Please enter a valid password"})
+                res.status(400).json({msg: "Invalid password for the above username! Please enter a valid password"});
             }
         }
         else{
-            res.status(404).json({msg: "No such user found in the database"})
+            res.status(404).json({msg: "No such user found in the database"});
         }
     } catch (error) {
-        res.status(500).json({error: error})
-        console.log(error)
+        res.status(500).json({error: error});
+        console.log(error);
     }   
 }
 
