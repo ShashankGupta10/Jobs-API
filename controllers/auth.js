c32704bc0d0cf4dc618cc215acd93f4075b35021
Shashank Gupta <shashankgupta9248@gmail.com>
2023-04-29 13:18:48 +0530

    finql commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,47 +1,44 @@
-const User = require('../models/User');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const secret = process.env.JWT_SECRET;
 
-const register = async (req, res) =>{
-    try {
-        const { name, email, password } = req.body;
-        const user = await User.create({
-            name: name,
-            email: email,
-            password: password
-        });
+const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.create({
+      name: name,
+      email: email,
+      password: password,
+    });
 
-        res.status(200).json({msg: "User created"});
+    res.status(200).json({ msg: "User created" });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
 
-    } catch (error) {
-        res.status(500).json({error: error});
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const loggedInUser = await User.findOne({ email: email });
+    if (loggedInUser) {
+      const isCorrect = await loggedInUser.comparePassword(password);
+
+      if (isCorrect) {
+        const token = loggedInUser.createJWT();
+        res.status(200).json({ msg: "User Logged In Successfully!", token });
+      }
+    } else {
+      res.status(404).json({ msg: "No such user found in the database" });
     }
-}
-
-const login = async (req, res) =>{
-    try {
-        const { email, password } = req.body;
-        const loggedInUser = await User.findOne({email: email});
-        if(loggedInUser){    
-            const isCorrect = await loggedInUser.comparePassword(password)
-
-            if(isCorrect){
-                const token = loggedInUser.createJWT()
-                res.status(200).json({msg: "User Logged In Successfully!", token})
-            }
-        }
-        else{
-            res.status(404).json({msg: "No such user found in the database"});
-        }
-    } catch (error) {
-        res.status(500).json({error: error});
-    }   
-}
-
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
 
 module.exports = {
-    register,
-    login
-}
+  register,
+  login,
+};
