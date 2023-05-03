dd6ecd35d8a370595cef1fc31dcd02bb64be9f4d
Shashank Gupta <shashankgupta9248@gmail.com>
2023-05-03 18:04:59 +0530

    final commit

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,20 +1,20 @@
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-require('dotenv').config()
-const secret = process.env.JWT_SECRET
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const secret = process.env.JWT_SECRET;
 
-const authenticationMiddleware = async (req, res, next) =>{
-    try {
-        const authHeader = req.headers.authorization
-        const token = authHeader.split(' ')[1]
-        if(token){
-            const payload = jwt.verify(token, secret)
-            req.user = {payload}
-    next()
-        }
-    } catch (error) {
-        console.log(error)
+const authenticationMiddleware = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    const token = authHeader.split(" ")[1];
+    if (token) {
+      const payload = jwt.verify(token, secret);
+      req.user = { payload };
+      next();
     }
-}
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-module.exports = authenticationMiddleware
+module.exports = authenticationMiddleware;
